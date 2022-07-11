29ec78523f8c0a6c7b0a85396ee84de80e9bce88
riceroc <eroces@protonmail.com>
2022-07-11 12:06:39 -0400

    feat: added eks

--- eks-variables.tf
+++ eks-variables.tf
@@ -3,5 +3,33 @@
 variable "cluster_name" {
     description = "Name of the EKS cluster"
     type = string
-    default = "terra-eks-demo"
+    default = "eks-demo"
+}
+
+variable "cluster_service_ipv4_cidr" {
+    description = "IPv4 CIDR for the kubernetes cluster"
+    type = string
+    default = null
+}
+
+variable "cluster_version" {
+    description = "k8s minor version to use for the EKS cluster"
+    type = string
+    default = null  
+}
+
+variable "cluster_endpoint_private_access" {
+    type = bool
+    default = false
+}
+
+variable "cluster_endpoint_public_access" {
+    type = bool 
+    default = true
+}
+
+variable "cluster_endpoint_public_access_cidrs" {
+    description = "List of CIDR blocks which can access EKS public resources"
+    type = list(string)
+    default = [ "0.0.0.0/0" ]
 }
--- eks.auto.tfvars
+++ eks.auto.tfvars
@@ -1,1 +1,6 @@
-cluster_name = "terra-eks-demo"
+cluster_name = "eksdemo"
+cluster_service_ipv4_cidr = "172.20.0.0/16"
+cluster_version = "1.22.10"
+cluster_endpoint_private_access = false
+cluster_endpoint_public_access = true
+cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
